4e411884194830acf5da01e1ac1fcbddea197ba4
TrexB <trexb22@gmail.com>
2024-05-12 15:39:19 +0530

    Created a sample scene for demonstration purposes.

---
+++ Assets/TrexB/CypherUtility/Sample/Scripts/SaveDataFormat.cs
@@ -0,0 +1,12 @@
+namespace TrexB.CipherUtility.Sample
+{
+    public class SaveDataFormat
+    {
+        public string PlayerName;
+        public int Health;
+        public int Level;
+        public bool HasUnlockedPistol;
+        public bool HasUnlockedShotGun;
+        public bool HasUnlockedBomb;
+    } // class
+}
