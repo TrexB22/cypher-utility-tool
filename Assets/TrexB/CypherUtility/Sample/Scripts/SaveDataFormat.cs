7602fb8c2fdfeccb1e8ec74c2d331421ca4779ab
TrexB <trexb22@gmail.com>
2024-05-19 20:59:25 +0530

    Comments were added to SaveDataFormat.cs to provide clarification.

--- Assets/TrexB/CypherUtility/Sample/Scripts/SaveDataFormat.cs
+++ Assets/TrexB/CypherUtility/Sample/Scripts/SaveDataFormat.cs
@@ -1,5 +1,12 @@
+using Unity.VisualScripting;
+
 namespace TrexB.CipherUtility.Sample
 {
+    // [System.Serializable] is an attribute in C# that indicates a class or a struct can be serialized, which means converting its instances into a format that
+    // can be easily stored or transmitted (e.g., to a file or over a network) and later reconstructed.
+    // The JsonUtility class in Unity does not require [System.Serializable] to work.
+    // It's still a good practice to use it for clarity and compatibility with other serialization mechanisms
+    [System.Serializable]
     public class SaveDataFormat
     {
         public string PlayerName;
